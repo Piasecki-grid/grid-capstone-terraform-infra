e13e62aed6f973c55ff276f9a2888e8b338eb386
Piasecki-grid <kpiasecki@griddynamics.com>
2025-03-28 09:29:36 +0100

    Changing infra v2

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 resource "azurerm_resource_group" "grid_terraform" {
-  name     = "example-resources"
+  name     = "example-resources-afsf"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "grid_vnet" {
-  name                = "grid-vnet"
+  name                = "grid-vnet-1242134§"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.grid_terraform.location
   resource_group_name = azurerm_resource_group.grid_terraform.name
 }
 
 resource "azurerm_subnet" "subnet_1" {
-  name                 = "internal"
+  name                 = "internal123123"
   resource_group_name  = azurerm_resource_group.grid_terraform.name
   virtual_network_name = azurerm_virtual_network.grid_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -19,12 +19,12 @@ resource "azurerm_subnet" "subnet_1" {
 
 
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+  name                = "example-nic-123131"
   location            = azurerm_resource_group.grid_terraform.location
   resource_group_name = azurerm_resource_group.grid_terraform.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "example-nic-123131"
     subnet_id                     = azurerm_subnet.subnet_1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
@@ -32,7 +32,7 @@ resource "azurerm_network_interface" "example" {
 }
 
 resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "vm-public-ip"
+  name                = "vm-public-ip-12312"
   resource_group_name = azurerm_resource_group.grid_terraform.name
   location            = azurerm_resource_group.grid_terraform.location
   allocation_method   = "Static"
