eb6f4db2b50b2a8e5b454b7bd0c72567d429faf7
Piasecki-grid <kpiasecki@griddynamics.com>
2025-03-28 10:54:17 +0100

    Adding storage account

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
   
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestorageaccount"
+    storage_account_name = "tfstatestorageaccount25"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
