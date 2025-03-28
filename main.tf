c2f0b72faa49de071a9df636a01447cd12e94bea
Piasecki-grid <kpiasecki@griddynamics.com>
2025-03-28 11:05:10 +0100

    Adding storage account

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# providers.tf
 terraform {
   required_providers {
     azurerm = {
@@ -17,46 +16,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
-
-# variables.tf
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "eastus"
-}
-
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-  default     = "dev"
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-  default     = "my-terraform-rg"
-}
-
-# main.tf
-resource "azurerm_resource_group" "example" {
-  name     = "${var.resource_group_name}-${var.environment}"
-  location = var.location
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "mystorageaccount${var.environment}"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-# outputs.tf
-output "resource_group_name" {
-  value = azurerm_resource_group.example.name
-}
-
-output "storage_account_name" {
-  value = azurerm_storage_account.example.name
 }
