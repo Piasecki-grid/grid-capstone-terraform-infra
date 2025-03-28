b6c0d59a66dc74c24f6fa4f60bcb2c34df9ba95f
Piasecki-grid <kpiasecki@griddynamics.com>
2025-03-28 09:25:37 +0100

    Changing infra

--- main.tf
+++ main.tf
@@ -39,31 +39,3 @@ resource "azurerm_public_ip" "vm_public_ip" {
 }
 
 
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.grid_terraform.name
-  location            = azurerm_resource_group.grid_terraform.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("~/.ssh/terraform-grid.pub")
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
