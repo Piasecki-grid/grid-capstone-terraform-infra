70d82adfa91e8de60faadb5eed1b402d7c2b456e
Kacper Piasecki <k.r.piasecki2@gmail.com>
2025-03-31 10:40:46 +0000

    Refactored syntax and structure in .tf files

--- random.tf
+++ random.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "example" {
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplestrggg"
-  resource_group_name       = azurerm_resource_group.example.name
+  resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
-  account_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
